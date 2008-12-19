1305390e8e5cda10e4b3da0f5c35b74e28ecdad1
campbellbarton <campbellbarton@25262666-f3c1-dd11-aff7-0030488c597c>
2008-12-19 15:06:01 +0000

    Adding utilities to build a compressed yo-frankie, useful for linux packages

---
+++ Makefile
@@ -0,0 +1,25 @@
+BLENDER = blender
+PYTHON = python
+SVN = svn
+PACKAGE_DIR = ./package
+
+all:
+	# Copy files into a package dir
+	rm -rf $(PACKAGE_DIR)
+	
+	# copy with svn export or normal copy if this is not a checkout
+	if test -d .svn; \
+	then $(SVN) export . $(PACKAGE_DIR); \
+	else mkdir ./package; cp -R audio chars effects hud levels menus props textures $(PACKAGE_DIR)/; \
+	fi
+	# end shell command
+	
+	
+	# Convert images, correct blendfile paths
+	$(PYTHON) dist/imagefile_compress.py $(BLENDER) $(PACKAGE_DIR)
+	
+	# Compress all blendfiles
+	$(PYTHON) dist/blendfile_gzip.py $(PACKAGE_DIR)
+	
+	
+	
---
+++ dist/imagefile_relink.py
@@ -0,0 +1,27 @@
+# This script must run inside blender
+import Blender
+import bpy
+
+
+def replace_ext(path, ext):
+	return '.'.join( path.split('.')[:-1] ) + '.' + ext
+
+change = False
+
+for i in bpy.data.images:
+	if i.lib==None:
+		filename = i.filename
+		filename_abs = Blender.sys.expandpath(filename)
+		
+		if not Blender.sys.exists(filename_abs):
+			filename_jpg = replace_ext(filename_abs, 'jpg')
+			if Blender.sys.exists(filename_jpg):
+				print "\tfixed path", filename_jpg
+				i.filename = replace_ext(filename, 'jpg')
+				change = True
+		
+
+if change:
+	filename = Blender.Get('filename')
+	print "Saving", filename
+	Blender.Save(filename, True) # True==Overwrite
