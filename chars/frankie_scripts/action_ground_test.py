8cb7c3a86c5ebc38b1f4148b629bc5fb8b44cdcd
campbellbarton <campbellbarton@25262666-f3c1-dd11-aff7-0030488c597c>
2010-02-10 21:02:27 +0000

    gpl headers

--- chars/frankie_scripts/action_ground_test.py
+++ chars/frankie_scripts/action_ground_test.py
@@ -1,3 +1,22 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+
 '''
 its a pitty we need this script at all, but knowing if we are on the ground or not isnt that simple
 basically use ray and collision sensors to detect of we are on the ground, keeping the "grounded" property up to date
